bceedbecb57273e32d9f1cce1b8a19d4d2e6e762
weaps <weaps@qq.com>
2020-07-05 23:48:58 +0800

    example svg

---
+++ src/icons/example.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>example</div>
+</template>
+<script>
+const req = require.context('./svg', false, /\.svg$/)
+const files = []
+req.keys().map(item => {
+  files.push(item)
+})
+export default {
+  name: 'example-svg',
+  created() {
+    console.log(files)
+  }
+}
+</script>
